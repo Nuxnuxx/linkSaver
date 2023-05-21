5cf1d4ebf134fdd2cef7675695d83736f09b0da3
gaetan <Nuxnuxx>
2023-05-21 07:22:54 +0200

    deuxieme version css finish

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './item.css'
 import DeleteItem from '../DeleteItem/DeleteItem';
+import fioriture from '../../assets/img/fioriture.png'
 
 const Item: React.FC<ItemProps> = ({ title, link }) => {
   return (
     <div className="card">
+      <img className='little-shiba-right' src={fioriture} alt='cat'/>
+      <img className='little-shiba-left' src={fioriture} alt='cat'/>
+      <img className='little-shiba-right-bottom' src={fioriture} alt='cat'/>
+      <img className='little-shiba-left-bottom' src={fioriture} alt='cat'/>
       <a className='a' href={link} target="_blank" rel="noopener noreferrer">
       <div className='card-layout'>
         <div className="card-title">{title}</div>
